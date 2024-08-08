1080301e089c8bb96c93a764b7fbef2a5ce305f2
philippstampfer <philipp.stampfer@protonmail.ch>
2024-08-08 18:41:42 +0200

    Add light attack base

--- Source/BisonHeart/Private/BisonHeartGameplayTags.cpp
+++ Source/BisonHeart/Private/BisonHeartGameplayTags.cpp
@@ -13,9 +13,16 @@ namespace BisonHeartGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_EquipAxes, "InputTag.EquipAxes");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipAxes, "InputTag.UnquipAxes");
 
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_LightAttack_Axes, "InputTag.LightAttack.Axes");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack_Axes, "InputTag.HeavyAttack.Axes");
+
 	/** Player Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Axes, "Player.Ability.Equip.Axes");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Unequip_Axes, "Player.Ability.Unequip.Axes");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light_Axes, "Player.Ability.Attack.Light.Axes");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Heavy_Axes, "Player.Ability.Attack.Heavy.Axes");
+
 
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe_Left, "Player.Weapon.Axe.Left");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe_Right, "Player.Weapon.Axe.Right");
